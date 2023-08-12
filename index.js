cfb3d49a19dd3cf92e62353f9c0d9afd239ff66f
Fentan-Kyc <di-ma02@mail.ru>
2023-08-12 16:49:44 +0300

    helper button on bottom

--- index.js
+++ index.js
@@ -13,13 +13,14 @@
 // @grant GM.xmlHttpRequest
 // ==/UserScript==
 
-const TOAST_DURATION = 10 * 1000
+const TOAST_DURATION = 10 * -1
 
 
 
 
 var accessToken;
 var canvas = document.createElement('canvas');
+var button = document.createElement('button');
 
 (async function() {
     GM_addStyle(GM_getResourceText('TOASTIFY_CSS'));
@@ -28,7 +29,7 @@ var canvas = document.createElement('canvas');
 	canvas.style.display = 'none';
 	canvas = document.body.appendChild(canvas);
 
-	
+
 	Toastify({
 		text: 'Obtaining access token...',
 		duration: TOAST_DURATION
@@ -38,10 +39,31 @@ var canvas = document.createElement('canvas');
 		text: `Obtained access token ${accessToken}!`,
 		duration: TOAST_DURATION
 	}).showToast();
-    
+    createButton();
+
 })();
 
 async function getAccessToken() {
     const url = window.location.href;
 	return url.split('/')[4];
+}
+
+function createButton(){
+    button.className = "YandexHelper__Button";
+    button.innerText = "HELPER";
+    button.style.display = "inline-block";
+    button.style.bottom = "13px";
+    button.style.position = "fixed";
+    button.style.right = "10px";
+    button.style.zIndex = 2147483647;
+    button.style.textDecoration = "none";
+    button.style.font = "inherit"
+    button.style.background = "linear-gradient(135deg,#73a5ff,#5477f5)";
+    button.style.cursor = "pointer"
+
+    button = document.body.appendChild(button);
+
+    button.onclick = function(){
+        alert("!");
+    }
 }
